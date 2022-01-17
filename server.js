d781096cb26a4d682e0abd4e3947eaec5d6f2729
aryatuhaearnest <aryatuhaearnest@gmail.com>
2022-01-17 22:00:47 +0300

    added a middle ware using morgan

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan')
 
 //route files
-const bootcamps =require('./routes/bootcamps')
+const bootcamps = require('./routes/bootcamps')
 
 //load env variables
-dotenv.config({path:'./config/config.env'})
+dotenv.config({ path: './config/config.env' })
 
 const app = express()
-  
+// dev logging middle ware
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+
+}
+
 // mount roters
-app.use('/api/v1/bootcamps',bootcamps)
+app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT,console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
 
